de8a4b461a9c9af497f17049aa1a3387935d90c5
ImaneToulni <32387310+ImaneToulni@users.noreply.github.com>
2020-02-10 22:52:12 +0100

    Add files via upload
    Les 1/
    - oefening met punten (zo goed als klaar, nog enkel de filter afwerken)

---
+++ SolutionLes1/WpfPunten/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPunten
+{
+    /// <summary>
+    /// Auteur: Imane Touln
+    /// Datum: 10/02/2020
+    /// Omschriijving: Puntenscore in een lijstje kunnen toevoegen en verwijderen
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            // Alle inputs verwijderen wanneer er op btn verwijderen gedrukt wordt
+            txtNaam.Clear();
+            txtPunten.Clear();
+            txtFilter.Clear();
+            lstbx.Items.Clear(); // om listbox te verwijderen, wordt items gebruikt
+
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            // items telkens toevoegen in een listbox
+            lstbx.Items.Add(txtNaam.Text + " - " + txtPunten.Text + " /100");
+
+        }
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            foreach (ListBoxItem item in lstbx.Items)
+            {
+               
+            }
+
+        }
+
+       
+    }
+}
