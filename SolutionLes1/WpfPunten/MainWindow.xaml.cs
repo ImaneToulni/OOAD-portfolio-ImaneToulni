c67e35ebbc186a08b253f42c9bfcbf67dac5e474
ImaneToulni <32387310+ImaneToulni@users.noreply.github.com>
2020-02-16 21:21:35 +0100

    Add files via upload
    TO DO
    1) TIMER
    - kleurovergang moet horizontaal 
    - reset min
    
    2) Punten
    - filter coderen

--- SolutionLes1/WpfPunten/MainWindow.xaml.cs
+++ SolutionLes1/WpfPunten/MainWindow.xaml.cs
@@ -16,10 +16,11 @@ using System.Windows.Shapes;
 namespace WpfPunten
 {
     /// <summary>
-    /// Auteur: Imane Touln
+    /// Auteur: Imane Toulni
     /// Datum: 10/02/2020
     /// Omschriijving: Puntenscore in een lijstje kunnen toevoegen en verwijderen
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -35,7 +36,6 @@ namespace WpfPunten
             txtPunten.Clear();
             txtFilter.Clear();
             lstbx.Items.Clear(); // om listbox te verwijderen, wordt items gebruikt
-
         }
 
         private void btnToevoegen_Click(object sender, RoutedEventArgs e)
@@ -43,17 +43,21 @@ namespace WpfPunten
             // items telkens toevoegen in een listbox
             lstbx.Items.Add(txtNaam.Text + " - " + txtPunten.Text + " /100");
 
+
+            // input verwijderen wanneer naam en punten toegevoegd wordt, zodat men direct een tweede naam en punt kan invoeren
+            txtNaam.Clear();
+            txtPunten.Clear();
         }
+
+        
+        // ********************************************* FILTER NOG NIET GEDAAN 
 
         private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
         {
-            foreach (ListBoxItem item in lstbx.Items)
-            {
-               
-            }
+            
 
         }
 
-       
     }
+
 }
---
+++ SolutionLes1/WpfTimer/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading; // toevoegen om gebruik te maken van de DispatcherTimer
+
+
+    namespace WpfTimer
+{
+    /// <summary>
+    /// Auteur: Imane Toulni
+    /// Datum: 10/02/2020
+    /// Omschriijving: timer oefeniing (min en seconden)
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer(); // DispatcherTimer is een klasse die gebruikt maak van Tick
+        private int count = 0; // standaard op nul zetten
+        int minuten = 0; // variabel standaard op nul zetten 
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        //*********************************************************************************************************************************************
+
+        //                                                  ****   TIIMER    ******
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            count++; // toenemen van min en sec
+            rectangleSec.Height += 10; // bij runnen gaat het automatisch beneden vergroten => aangepast in XAML VerticalAlignment="Bottom"
+            if (count == 3) // 60 sec = 1 min
+            {
+                count = 0;
+                minuten++;
+                rectangleMin.Height = (10 * minuten);
+                rectangleSec.Height = 0;
+            }
+            lblMinutenEnSeconden.Content = $"{minuten} minuten {count} seconden"; // in de label zal het aantal min en seconden automatiisch toenemen
+        }
+
+        // toevoegen om geen error melding te hebben
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        //*********************************************************************************************************************************************
+
+        //                                                  ****   ALLE KNOPPEN    ****
+        // wanneer er op START gedrukt wordt, zullen de min en sec toenemen
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblMinutenEnSeconden.Content = count; 
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        // wanneer er op STOP gedrukt wordt, wordt er effectief gestopt
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        // RESET min en seconden 
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            lblMinutenEnSeconden.Content = "";
+            rectangleMin.Height = 0;
+            rectangleSec.Height = 0;
+         
+        }
+    }
+}
